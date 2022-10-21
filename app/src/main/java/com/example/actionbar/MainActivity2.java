b7e709944e40bab5dc2ac810dd1f00f02e120633
Salman <mohdsalman309846@gmail.com>
2022-10-21 18:53:50 +0530

    ActionBar for Android

---
+++ app/src/main/java/com/example/actionbar/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.actionbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.chrome);
+        actionBar.setTitle(" Second Welcome");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+}
